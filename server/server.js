d29f9d8fe43652636ad10af8a21ef35a054f0958
Simon Ding <SimonDing87@gmail.com>
2016-07-27 17:05:55 -0700

    add basic server and client template

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var path = require('path');
+var app = express();
+
+var TEXT_SOURCE = './server/data/coding_challenge_data_set.txt';
+
+// serve static files
+app.use(bodyParser.json());
+app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static('client'));
+
+// listen
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port ' + port + '...');
+});
