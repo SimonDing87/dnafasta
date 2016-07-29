6f9f9439ed366c22960092653026e367ef85c996
Simon Ding <SimonDing87@gmail.com>
2016-07-28 17:11:07 -0700

    add basic client ui

--- client/app.js
+++ client/app.js
@@ -2,17 +2,21 @@ angular.module('MyApp', [])
   .controller('MainController', ['$http', '$scope', function($http, $scope) {
     console.log('MainController loaded');
 
-    $scope.message = "May take 15-20 seconds."
+    $scope.message = "Get Sequence!";
     $scope.working = false;
     $scope.get = function() {
+      $scope.message = "Processing...(may take 30s~)";
+      var start = Date.now();
       $scope.working = true;
       $http({
           method: 'GET',
           url: '/test'
         }).then(function(data) {
           $scope.working = false;
-          console.log(data.data);
+          $scope.result = data.data;
+          $scope.message = "Completed in " + (Date.now() - start) / 1000 + "seconds";
         });
     }
 
+    $scope.result = "";
   }]);
